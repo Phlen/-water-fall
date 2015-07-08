57bfff184faa5b2e7460bcafc65d0068c3ffda97
Allen <735016387@qq.com>
2015-07-08 10:28:38 +0800

    changes log

--- Js/heart.js
+++ Js/heart.js
@@ -3,32 +3,32 @@
  */
 
 /*
-$(document).ready(function(){
+ $(document).ready(function(){
+ $('.heart').toggle(
+ function(){
+ $(this).addClass('red');
+ },
+ function(){
+ $(this).removeClass('red');
+ }
+ );
+ });*/
+
+$(document).ready(function () {
+
+  /* var container = document.querySelector('#masonry');
+   var msnry = new Masonry( container, {
+   // options...
+   itemSelector: '.pin',
+   columnWidth: 225
+   });*/
+
   $('.heart').toggle(
-      function(){
-        $(this).addClass('red');
+      function () {
+        $(this).css('color', 'red');
       },
-      function(){
-        $(this).removeClass('red');
-      }
-  );
-});*/
-
-$(document).ready(function(){
-
- /* var container = document.querySelector('#masonry');
-  var msnry = new Masonry( container, {
-    // options...
-    itemSelector: '.pin',
-    columnWidth: 225
-  });*/
-
-  $('.heart').toggle(
-      function(){
-        $(this).css('color','red');
-      },
-      function(){
-        $(this).css('color','#999999');
+      function () {
+        $(this).css('color', '#999999');
       }
   );
 });
